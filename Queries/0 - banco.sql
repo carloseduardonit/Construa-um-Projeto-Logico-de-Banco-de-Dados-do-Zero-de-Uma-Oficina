58fe1f03bca8bba796409e986fdca423bc8bd26a
Carlos Eduardo dos Santos Figueiredo <42427944+carloseduardonit@users.noreply.github.com>
2025-08-05 00:18:02 -0300

    Utualização  do Script do Banco.

--- Queries/0 - banco.sql
+++ Queries/0 - banco.sql
@@ -1,5 +1,9 @@
-# 0 - banco.sql script para criar o banco de dados
+# 0 - banco.sql 
+# script para criar o banco de dados da oficina
 create  database if not exists bd_oficina;
+# certifica se todos bancos criados
 show databases;
+# seleciona o banco de dados
 use bd_oficina;
+# certifica se todas tabelas criadas
 show tables;
